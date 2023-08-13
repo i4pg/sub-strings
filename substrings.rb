8f3a7681fb05ce4e0dc1d28493b3bb8c78156930
i4pg <86368009+i4pg@users.noreply.github.com>
2023-08-13 09:22:22 +0400

    feat: update script

--- substrings.rb
+++ substrings.rb
@@ -1,14 +1,17 @@
-dictionary = %w[below down go going horn how howdy it i low own part partner sit]
-def substrings(text, array)
-  result = array.each_with_object({}) do |str, acc|
-    text.split(' ').each do |word|
-      next unless word.include?(str)
+# frozen_string_literal: true
 
-      acc[str] = 0 unless acc[str]
-      acc[str] += 1
-    end
-  end
-  p result
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+def substrings(text, dictionary)
+  dictionary.each_with_object({}) do |word, match|
+    match[word] = text
+                  .scan(/\w{2,}/)
+                  .join
+                  .downcase
+                  .scan(/#{word}/)
+                  .length
+  end.select { |_word, match| match.positive? }
 end
-# substrings('below', dictionary)
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+
+substrings('below', dictionary)
+substrings("Howdy partner, sit down! How's it going? ", dictionary)
